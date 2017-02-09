d3ca9c7f848a819b6f8900d85a3b5bf5ece2fe91
wbrooks1 <wbrooks1@uw.edu>
2017-02-08 17:42:43 -0800

    Started NewIncident screen.

---
+++ app/config/mandatoryComponentList.js
@@ -0,0 +1,14 @@
+export const components = {
+    medical: {
+        "title" : ["text", "Title", "Enter incident title"],
+        "description" : ["multi_text", "Description", "Enter incident description"]
+    },
+    natural : {
+        "title" : ["text", "Title", "Enter incident title"],
+        "description" : ["text", "Description", "Enter incident description"]
+    },
+    military : {
+        "title" : ["text", "Title", "Enter incident title"],
+        "description" : ["text", "Description", "Enter incident description"]
+    }
+};
--- app/screens/NewIncident/NewIncident.js
+++ app/screens/NewIncident/NewIncident.js
@@ -1,13 +1,21 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, Image, View, TextInput, ScrollView, Navigator, BackAndroid} from 'react-native'
+import {AppRegistry, StyleSheet, Text, Image, View, TextInput, ScrollView, Navigator, BackAndroid,
+ListView} from 'react-native'
 import styles from './styles';
-
+import SingleLineInput from '../../components/SingleLineInput';
+import {componentList} from '../../config/mandatoryComponentList'
 
 export default class NewIncident extends Component {
     constructor() {
         super();
+        this.state = {
+            inputCompentents: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+
+        };
     }
 
     componentWillMount() {
@@ -18,6 +26,15 @@ export default class NewIncident extends Component {
             }
             return false;
         });
+        this.setState({inputCompentents: componentList});
+    }
+
+    renderComponents(component) {
+        if (component.type === "text") {
+            return (
+                <SingleLineInput title={component.title[1]}/>
+            )
+        }
     }
 
     render() {
@@ -29,6 +46,8 @@ export default class NewIncident extends Component {
                 <Text style = {styles.title }>
                     New Incident
                 </Text >
+                <ListView dataSource={this.state.inputCompentents}
+                          renderRow={this.renderComponents(this)}/>
             </View>
         );
     }
